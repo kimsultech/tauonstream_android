f994475debd32524a8721df6f28950061d78cead
sultannamja <ssx20010531@gmail.com>
2020-10-07 01:56:18 +0700

    Remove live Chat, Remove Unused Library

--- app/src/main/java/com/kangtech/tauonstream/MainActivity.java
+++ app/src/main/java/com/kangtech/tauonstream/MainActivity.java
@@ -1,97 +1,50 @@
 package com.kangtech.tauonstream;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.cardview.widget.CardView;
-import androidx.coordinatorlayout.widget.CoordinatorLayout;
-import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
-import androidx.core.content.ContextCompat;
-import androidx.core.widget.NestedScrollView;
-import androidx.drawerlayout.widget.DrawerLayout;
-
-
-import android.accessibilityservice.AccessibilityService;
 import android.annotation.SuppressLint;
 import android.app.Dialog;
-import android.app.Notification;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.ComponentName;
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.ServiceConnection;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
-import android.inputmethodservice.Keyboard;
-import android.inputmethodservice.KeyboardView;
-import android.media.Image;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.IBinder;
-import android.text.Editable;
 import android.text.Html;
-import android.text.TextWatcher;
 import android.util.Base64;
 import android.util.Log;
-import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
-import android.widget.ScrollView;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.airbnb.lottie.LottieAnimationView;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.drawerlayout.widget.DrawerLayout;
+
 import com.google.android.exoplayer2.MediaItem;
-import com.google.android.exoplayer2.Player;
 import com.google.android.exoplayer2.SimpleExoPlayer;
 import com.google.android.gms.oss.licenses.OssLicensesMenuActivity;
 import com.google.android.material.navigation.NavigationView;
-import com.kangtech.sc_client.Ack;
-import com.kangtech.sc_client.BasicListener;
-import com.kangtech.sc_client.Emitter;
-import com.kangtech.sc_client.ReconnectStrategy;
-import com.kangtech.sc_client.Socket;
 import com.kangtech.tauonstream.api.ApiServiceInterface;
 import com.kangtech.tauonstream.model.data.musicModel;
 import com.kangtech.tauonstream.model.data.updateModel;
 import com.kangtech.tauonstream.util.Server;
 import com.kangtech.tauonstream.util.SharedPreferencesUtils;
-import com.neovisionaries.ws.client.WebSocketException;
-import com.neovisionaries.ws.client.WebSocketFrame;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.List;
-import java.util.Map;
-
-import hani.momanii.supernova_emoji_library.Actions.EmojIconActions;
-import hani.momanii.supernova_emoji_library.Helper.EmojiconEditText;
-import hani.momanii.supernova_emoji_library.Helper.EmojiconTextView;
 import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
-
-import static android.graphics.Typeface.BOLD;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -101,7 +54,6 @@ public class MainActivity extends AppCompatActivity {
     private MediaItem mediaItem;
 
     private LinearLayout llRefresh;
-    private LottieAnimationView lavRefresh;
 
     private TextView tvStation;
 
@@ -122,34 +74,11 @@ public class MainActivity extends AppCompatActivity {
     DrawerLayout drawer;
     NavigationView navigationView;
 
-    int portOri = Integer.parseInt(SharedPreferencesUtils.getString("port", "7501"));
-    int port = portOri + 1;
-
-    String url="ws://" + Server.BASE_URL + ":" + port + "/socketcluster/";
-    Socket socket;
-    EmojiconEditText emojiconEditText;
-    ImageView emojiButton;
-    NestedScrollView scrollView;
-    ImageView submitButton;
-    View rootView;
-    LinearLayout container;
-    EmojIconActions emojIcon;
-    EditText username;
-    String Username="<font color=\"#ff00000\">Demo</font>";
-    Handler Typinghandler=new Handler();
-    Boolean typing=false;
-
     TextView tvNotSupportChat;
 
     private TextView tvOnline;
-    private String countOnline;
 
     private Button btnJoin;
-    boolean keyboardOpen = false;
-
-    //Bind/Unbind music service
-    private boolean mIsBound = false;
-    private StreamService mServ;
 
     public MainActivity() {
     }
@@ -167,7 +96,6 @@ public class MainActivity extends AppCompatActivity {
         tvStation.setText(SharedPreferencesUtils.getString("ip", "Station"));
 
         llRefresh = findViewById(R.id.ll_nav_refresh);
-        lavRefresh = findViewById(R.id.lav_refresh);
         ivCoverAlbum = findViewById(R.id.iv_cover_album);
         tvTitleSong = findViewById(R.id.tv_title_song);
         tvLyrics = findViewById(R.id.tv_lyrics);
@@ -176,7 +104,6 @@ public class MainActivity extends AppCompatActivity {
 
         pbSong = findViewById(R.id.pb_song);
 
-        tvOnline = findViewById(R.id.tv_online);
 
         btnJoin = findViewById(R.id.btn_join);
 
@@ -187,20 +114,6 @@ public class MainActivity extends AppCompatActivity {
 
         player = new SimpleExoPlayer.Builder(this).build();
 
-        player.addListener(new Player.EventListener() {
-            @RequiresApi(api = Build.VERSION_CODES.N)
-            @Override
-            public void onIsPlayingChanged(boolean isPlaying) {
-                if (isPlaying) {
-
-                    if (player.getCurrentPosition() == 0) {
-
-                    }
-                } else {
-
-                }
-            }
-        });
 
         /*Intent Stream = new Intent(this, StreamService.class);
         Stream.putExtra("play", "play");
@@ -222,14 +135,11 @@ public class MainActivity extends AppCompatActivity {
 
         //customNotification();
 
-        liveChat();
-
 
         llRefresh.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                lavRefresh.playAnimation();
 
                 if (player.isPlaying()) {
                     player.stop(true);
@@ -549,294 +459,8 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
-        JSONObject object=new JSONObject();
-        try {
-            object.put("ismessage",false);
-            object.put("data","<b><gray>"+Username+"</b></gray> leaved the chat");
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        socket.publish("livechat",object);
-
     }
 
-    private void liveChat() {
-
-        socket = new Socket(url);
-
-        socket.setListener(new BasicListener() {
-
-            public void onConnected(Socket socket, Map<String, List<String>> headers) {
-                socket.createChannel("livechat").subscribe(new Ack() {
-                    @Override
-                    public void call(String name, Object error, Object data) {
-                        if (error==null){
-                            Log.e ("Success apaannnn","subscribed to channel "+ data);
-                        }
-                    }
-                });
-                Log.i("Success ","Connected to endpoint");
-
-                tvNotSupportChat.setVisibility(View.GONE);
-                btnJoin.setVisibility(View.VISIBLE);
-            }
-
-            public void onDisconnected(Socket socket, WebSocketFrame serverCloseFrame, WebSocketFrame clientCloseFrame, boolean closedByServer) {
-                Log.i("Success ","Disconnected from end-point");
-            }
-
-            public void onConnectError(Socket socket, WebSocketException exception) {
-                tvNotSupportChat.setVisibility(View.VISIBLE);
-                btnJoin.setVisibility(View.GONE);
-
-                Log.e("Success ","Got connect error "+ exception);
-            }
-
-            public void onSetAuthToken(String token, Socket socket) {
-                socket.setAuthToken(token);
-            }
-
-            public void onAuthentication(Socket socket,Boolean status) {
-                if (status) {
-                    Log.i("Success ","socket is authenticated");
-                } else {
-                    Log.i("Success ","Authentication is required (optional)");
-                }
-            }
-
-        });
-
-        /*
-        Socket.Channel channel = socket.getChannelByName("MyClassroom");
-        */
-        socket.setReconnection(new ReconnectStrategy().setMaxAttempts(10).setDelay(3000));
-
-        socket.connectAsync();
-
-        rootView = findViewById(R.id.root_view);
-        emojiButton = (ImageView) findViewById(R.id.emoji_btn);
-        emojiButton.setVisibility(View.GONE);
-
-        submitButton = (ImageView) findViewById(R.id.submit_btn);
-        submitButton.setVisibility(View.GONE);
-
-        container = (LinearLayout) findViewById(R.id.container);
-        scrollView =  findViewById(R.id.scroll);
-        emojiconEditText = (EmojiconEditText) findViewById(R.id.emojicon_edit_text);
-        emojiconEditText.setVisibility(View.GONE);
-
-        emojIcon = new EmojIconActions(this,rootView,emojiconEditText,emojiButton,"#495C66","#DCE1E2","#E6EBEF");
-        emojIcon.ShowEmojIcon();
-        emojIcon.setUseSystemEmoji(true);
-        emojIcon.setKeyboardListener(new EmojIconActions.KeyboardListener() {
-
-            @Override
-            public void onKeyboardOpen() {
-                keyboardOpen = true;
-            }
-
-            @Override
-            public void onKeyboardClose() {
-                Log.e("Keyboard","close");
-            }
-        });
-
-
-
-        View v = getLayoutInflater().inflate(R.layout.dialogue_layout, null);
-        username =  v.findViewById(R.id.username_input);
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-
-
-        btnJoin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                joinChat();
-            }
-
-            private void joinChat() {
-                builder.setView(v)
-                        .setCancelable(false)
-                        .setPositiveButton(R.string.join, new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int id) {
-
-                                if (username.getText().length() == 0 || username.getText().toString().equals("")) {
-                                    Toast.makeText(MainActivity.this, "Are you Sure?\ndid you forget something?", Toast.LENGTH_LONG).show();
-                                } else {
-                                    // sign in the user ...
-                                    Username = username.getText().toString();
-                                    JSONObject object=new JSONObject();
-                                    try {
-                                        object.put("ismessage",false);
-                                        object.put("data","<b><gray>"+Username+"</b></gray> joined to chat");
-                                    } catch (JSONException e) {
-                                        e.printStackTrace();
-                                    }
-                                    socket.publish("livechat", object);
-
-                                    submitButton.setVisibility(View.VISIBLE);
-                                    emojiButton.setVisibility(View.VISIBLE);
-                                    emojiconEditText.setVisibility(View.VISIBLE);
-
-                                    btnJoin.setVisibility(View.GONE);
-                                }
-
-                            }
-                        })
-                        .create()
-                        .show();
-            }
-        });
-
-        submitButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                JSONObject object=new JSONObject();
-                try {
-                    object.put("user",Username);
-                    object.put("ismessage",true);
-                    object.put("data",emojiconEditText.getText().toString());
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-
-                socket.publish("livechat", object, new Ack() {
-                    @Override
-                    public void call(String name, Object error, Object data) {
-                        if (error==null){
-                            Log.i ("Success","Publish sent successfully");
-                        }
-                    }
-                });
-
-                emojiconEditText.setText("");
-            }
-        });
-
-/*        emojiconEditText.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if (Username==null) return;
-
-                if (!typing){
-                    typing=true;
-                    if (!s.equals("")) {
-                        JSONObject object = new JSONObject();
-                        try {
-                            object.put("user", Username);
-                            object.put("istyping", true);
-                            object.put("data", "<b>" + Username + "</b> is typing...");
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                        socket.publish("livechat",object);
-                    }
-                }
-
-*//*                Typinghandler.removeCallbacks(onTypingTimeout);
-                Typinghandler.postDelayed(onTypingTimeout,600);*//*
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        });*/
-
-        socket.onSubscribe("livechat",new Emitter.Listener() {
-            @Override
-            public void call(String name, final Object data) {
-
-                try {
-                    JSONObject object= (JSONObject) data;
-                    if (object.opt("istyping") != null &&  !object.getString("user").equals(Username)){
-                       /* if (object.getBoolean("istyping")){
-                            final TextView textView=getTextView();
-                            textView.setText(Html.fromHtml(object.getString("data")));
-                            runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    container.addView(textView);
-                                    scrollView.scrollTo(0,scrollView.getBottom());
-                                }
-                            });
-                        }else{*/
-                            Log.i ("success","Stopped typing");
-                            runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    container.removeViewAt(container.getChildCount()-1);
-                                }
-                            });
-                        /*}*/
-
-                    } else {
-                        final TextView textView = getTextView();
-
-
-                                /*countOnline = String.valueOf(data);*/
-                        
-
-                        if (!object.getBoolean("ismessage")) {
-                            textView.setGravity(Gravity.CENTER_HORIZONTAL);
-                            textView.setText(Html.fromHtml(object.getString("data")), TextView.BufferType.SPANNABLE);
-                        } else {
-                            textView.setText(Html.fromHtml("<b>" + object.getString("user") + "</b> : " + object.getString("data")), TextView.BufferType.SPANNABLE);
-                        }
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                container.addView(textView);
-                                scrollView.scrollTo(0, scrollView.getBottom());
-
-                                /*tvOnline.setText(countOnline);*/
-                                //Log.e("Count Online",countOnline);
-                            }
-                        });
-
-                    }
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-    }
-
-    private EmojiconTextView getTextView(){
-        EmojiconTextView textView = new EmojiconTextView(MainActivity.this);
-        textView.setUseSystemDefault(false);
-        textView.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 16);
-        textView.setEmojiconSize(44);
-        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        params.setMargins(0, 5, 0, 5);
-        textView.setLayoutParams(params);
-        return textView;
-    }
-/*    private Runnable onTypingTimeout = new Runnable() {
-        @Override
-        public void run() {
-            if (!typing) return;
-
-            typing = false;
-//            mSocket.emit("stop typing");
-            JSONObject object=new JSONObject();
-            try {
-                object.put("user",Username);
-                object.put("istyping",false);
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-            socket.publish("livechat",object);
-        }
-    };*/
 
     @Override
     public void onBackPressed() {
